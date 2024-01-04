feb5559311f33e235d51c5902a3dac6454da8854
Alex22JAMES <91525156+Alex22JAMES@users.noreply.github.com>
2024-01-04 15:06:43 +0100

    creer / supprimer / modifier / lister des licenciés

---
+++ Modele/LicencieDAO.php
@@ -0,0 +1,77 @@
+<?php
+
+class LicencieDAO {
+    private $db;  // La connexion à la base de données
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function create(Licencie $licencie) {
+        $nom = $licencie->getNom();
+        $prenom = $licencie->getPrenom();
+        $numeroLicence = $licencie->getNumeroLicence();
+        $contactId = $licencie->getContactId();
+        $categorieId = $licencie->getCategorieId();
+
+        $query = "INSERT INTO licencies (nom, prenom, numero_licence, contact_id, categorie_id) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("ssiii", $nom, $prenom, $numeroLicence, $contactId, $categorieId);
+
+        return $stmt->execute();
+    }
+
+    public function update(Licencie $licencie) {
+        $id = $licencie->getId();
+        $nom = $licencie->getNom();
+        $prenom = $licencie->getPrenom();
+        $numeroLicence = $licencie->getNumeroLicence();
+        $contactId = $licencie->getContactId();
+        $categorieId = $licencie->getCategorieId();
+
+        $query = "UPDATE licencies SET nom=?, prenom=?, numero_licence=?, contact_id=?, categorie_id=? WHERE id=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("ssiiii", $nom, $prenom, $numeroLicence, $contactId, $categorieId, $id);
+
+        return $stmt->execute();
+    }
+
+    public function delete($id) {
+        $query = "DELETE FROM licencies WHERE id=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $id);
+
+        return $stmt->execute();
+    }
+
+    public function getById($id) {
+        $query = "SELECT * FROM licencies WHERE id=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $id);
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return new Licencie($row['id'], $row['nom'], $row['prenom'], $row['numero_licence'], $row['contact_id'], $row['categorie_id']);
+        }
+
+        return null;
+    }
+
+    public function getAll() {
+        $query = "SELECT * FROM licencies";
+        $result = $this->db->query($query);
+
+        $licencies = array();
+
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $licencies[] = new Licencie($row['id'], $row['nom'], $row['prenom'], $row['numero_licence'], $row['contact_id'], $row['categorie_id']);
+            }
+        }
+
+        return $licencies;
+    }
+}
--- controller/LicencieController.php
+++ controller/LicencieController.php
@@ -1,4 +1,44 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
+use App\Model\LicencieDAO;
+use App\Model\Licencie;
+use Twig\Environment;
+
+class LicencieController {
+    private $licencieDAO;
+
+    public function __construct(LicencieDAO $licencieDAO) {
+        $this->licencieDAO = $licencieDAO;
+    }
+
+    public function createLicencie($nom, $prenom, $numeroLicence, $contactId, $categorieId) {
+        // Créer un nouveau licencié
+        $licencie = new Licencie(null, $nom, $prenom, $numeroLicence, $contactId, $categorieId);
+        $this->licencieDAO->create($licencie);
+    }
+
+    public function updateLicencie($id, $nom, $prenom, $numeroLicence, $contactId, $categorieId) {
+        // Mettre à jour un licencié existant
+        $licencie = $this->licencieDAO->getById($id);
+        $licencie->setNom($nom);
+        $licencie->setPrenom($prenom);
+        $licencie->setNumeroLicence($numeroLicence);
+        $licencie->setContactId($contactId);
+        $licencie->setCategorieId($categorieId);
+        $this->licencieDAO->update($licencie);
+    }
+
+    public function deleteLicencie($id) {
+        // Supprimer un licencié
+        $this->licencieDAO->delete($id);
+    }
+
+    public function listLicencie() {
+        // Afficher la liste des licenciés
+        $licencies = $this->licencieDAO->getAll();
+        // Appeler une vue pour afficher les licenciés
+        include('view/Licencie/listLicencie.html.twig');
+    }
+}
